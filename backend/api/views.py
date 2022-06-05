580872e4672653168eb21345febdbf38b89ddc7b
choudharykartik <38472520+choudharykartik@users.noreply.github.com>
2022-06-05 11:56:49 +0530

    modelviewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -15,39 +15,9 @@ def home(request):
     return HttpResponse('Welcome to my Task App')
 
 
-class TaskList(viewsets.ViewSet):
+class TaskList(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer 
     
 
-    def list(self,request):
-        queryset = Task.objects.all()
-        serializer = TaskSerializer(queryset,many=True)
-        return Response(serializer.data)
-
-    def create(self,request):
-        serializer = TaskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
-    def retrieve(self,request,pk=None):
-        queryset = Task.objects.all()
-        task = get_object_or_404(queryset,pk=pk)
-        serializer = TaskSerializer(task)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-    def update(self,request,pk=None):
-        queryset = Task.objects.all()
-        task = get_object_or_404(queryset,pk=pk)
-        serializer = TaskSerializer(task,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_200_OK)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def destroy(self,request,pk=None):
-        queryset = Task.objects.all()
-        task = get_object_or_404(queryset,pk=pk)
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
