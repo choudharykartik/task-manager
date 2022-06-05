b69c511dd356d18a9b8fb9b2c3093022e23f6a58
choudharykartik <38472520+choudharykartik@users.noreply.github.com>
2022-06-05 11:15:56 +0530

    all operations and urls by viewsets and router

--- backend/api/views.py
+++ backend/api/views.py
@@ -26,23 +26,28 @@ class TaskList(viewsets.ViewSet):
     def create(self,request):
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
     def retrieve(self,request,pk=None):
-        queryset = Task.objects.get_object_or_404(pk=pky)
+        queryset = Task.objects.all()
         task = get_object_or_404(queryset,pk=pk)
         serializer = TaskSerializer(task)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def update(self,request,pk=None):
+        queryset = Task.objects.all()
+        task = get_object_or_404(queryset,pk=pk)
+        serializer = TaskSerializer(task,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self,request,pk=None):
+        queryset = Task.objects.all()
+        task = get_object_or_404(queryset,pk=pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-class TaskDetail(generics.GenericAPIView,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    lookup_field = 'id'
-    def get(self,request,id):
-        return self.retrieve(request,id)
-    def put(self,request,id):
-        return self.update(request,id)
-    def delete(self,request,id):
-        return self.destroy(request,id)
