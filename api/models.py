8083f08c3ba058106c0263833cd020a89f6616e1
choudharykartik <kartik123choudhary@gmail.com>
2022-07-17 08:03:42 +0530

    render user specific tasks

--- api/models.py
+++ api/models.py
@@ -9,6 +9,8 @@ class Task(models.Model):
     description = models.CharField(max_length=1000, null=True, blank=True)
     priority = models.IntegerField(default=5, null=True, blank=True)
     due_date = models.DateTimeField(blank=True, null=True)
+    created_by = models.ForeignKey(
+        "Appuser", on_delete=models.CASCADE, null=True, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
--- api/views.py
+++ api/views.py
@@ -20,6 +20,19 @@ class TaskList(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
+    def list(self, request):
+        queryset = Task.objects.filter(created_by=request.user)
+        serialized = TaskSerializer(queryset, many=True)
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        serialized = TaskSerializer(data=request.data)
+        if serialized.is_valid():
+            serialized.save(created_by=request.user)
+            return Response(serialized.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": serialized.error}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = Appuser.objects.all()
